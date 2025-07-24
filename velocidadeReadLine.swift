423f2ef5cb10b1a2c56ae7440ea5d11485f5d891
Daniela Alves <1999dani.alves@gmail.com>
2025-07-24 14:44:45 -0300

    Add files via upload

---
+++ velocidadeReadLine.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+// Aula 04 - Atividade Velocidade
+
+print("Digite a velocidade:")
+let velocidade = Int(readLine()!)!
+
+if velocidade > 60 {
+    print("Multado!")
+} else {
+    print("Não Multado.")
+}
